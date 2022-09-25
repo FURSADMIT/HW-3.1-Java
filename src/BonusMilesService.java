ae5101a4ef534061f9d172bce17189cd91850efd
Fursa <fursad14@mail.ru>
2022-09-25 22:30:36 +0300

    firs commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,10 @@
+public class BonusMilesService {
+    public int calculate(int price) {
+        int cost = 20;
+        int calculate;
+        calculate = price / cost;
+        
+        return calculate;
+    }
+}
+
